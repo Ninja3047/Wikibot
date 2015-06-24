c127edacbe674c82e7eeb9ca7bd99f2ddb10ed0d
William Tan <williamjefftan@gmail.com>
2015-06-24 15:01:35 -0400

    use the wikipedia api

--- wikibot.py
+++ wikibot.py
@@ -1,12 +1,15 @@
 #!/bin/env python
 
-from bs4 import BeautifulSoup
-import requests
+import urllib.request
+import urllib.parse
+import json
 
 import socket, ssl
 
+import traceback
+
 class WikiBot:
-    auth = False
+    wurl = "https://en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts&exchars=420&explaintext&redirects=&titles="
 
     def __init__(self, host, port, nick, ident, realname):
         self.host = host
@@ -16,6 +19,7 @@ class WikiBot:
         self.realname = realname
         context = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
         self.s=context.wrap_socket(socket.socket())
+        self.auth = False
 
     def getHost(self):
         return self.host
@@ -34,25 +38,33 @@ class WikiBot:
 
     def search(self, term):
         try:
-            wcontent = requests.get("https://en.m.wikipedia.org/w/index.php?search=%s" % term).text
-            soup = BeautifulSoup(wcontent).select("div > p")[0]
-            return soup.get_text()
+            req = urllib.request.Request(self.wurl+urllib.parse.quote(term))
+            req.add_header('User-Agent','WikiBot/2.0 ([email]) Simple IRC Bot')
+            url = urllib.request.urlopen(req).read()
+            content = json.loads(url.decode())['query']['pages']
+            return next(iter(content.values()))['extract']
         except:
-            return "Error not found"
+            print(traceback.format_exc())
+            return "Not found"
 
-    def send(self, data):
+    def sendraw(self, data):
+        print(data)
         self.s.send(data.encode())
 
+    def send(self, dest, data):
+        data = data.replace("\n"," ")
+        self.sendraw("PRIVMSG %s :%s\r\n" % (dest, data))
+
     def join(self, channel):
-        self.send("JOIN %s\r\n" % channel)
+        self.sendraw("JOIN %s\r\n" % channel)
 
     def identify(self, password):
-        self.send("PRIVMSG NICKSERV :IDENTIFY %s\r\n" % password)
+        self.sendraw("PRIVMSG NICKSERV :IDENTIFY %s\r\n" % password)
 
     def connect(self):
         self.s.connect((self.host, self.port))
-        self.send("NICK %s\r\n" % self.nick)
-        self.send("USER %s %s * :%s\r\n" % (self.ident, self.host, self.realname))
+        self.sendraw("NICK %s\r\n" % self.nick)
+        self.sendraw("USER %s %s * :%s\r\n" % (self.ident, self.host, self.realname))
         print("Connected to %s" % self.host)
 
     def run(self):
@@ -66,7 +78,7 @@ class WikiBot:
                 line=line.rstrip().split()
                 print(line)
                 if(line[0]=="PING"):
-                    self.send("PONG %s\r\n" % line[1])
+                    self.sendraw("PONG %s\r\n" % line[1])
                 elif(len(line) > 3):
                     if(line[1]=="PRIVMSG"):
                         if(self.auth==False):
@@ -75,13 +87,9 @@ class WikiBot:
 
                         if("#" in line[2] and ".wiki" in line[3]):
                             sterm = " ".join(line[4:])
-                            self.send("PRIVMSG %s :%s\r\n" % (line[2], self.search(sterm)))
+                            print(sterm)
+                            self.send(line[2], self.search(sterm))
                         elif("#" in line[3]):
                             self.join(line[3].replace(":",""))
                     elif(line[1]=="INVITE"):
                         self.join(line[3].replace(":",""))
-
-
-wbot = WikiBot("irc.rizon.net", 6697, "WikiBot", "WikiBot", "WikiBot")
-wbot.connect()
-wbot.run()
